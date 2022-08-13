d7b91bf06b14e544849565b592a77c5c81434a5d
nobuglady <nobuglady@protonmail.com>
2022-08-13 17:22:53 +0900

    create

---
+++ src/main/java/io/github/nobuglady/network/ui/LadybugflowUiApplication.java
@@ -0,0 +1,13 @@
+package io.github.nobuglady.network.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LadybugflowUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LadybugflowUiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nobuglady/network/ui/LadybugflowUiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nobuglady.network.ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LadybugflowUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
